eaa1a07cb55a356b71ff54299c7ce51576d4bce4
ese111 <feung94@gmail.com>
2022-03-04 13:57:16 +0900

    파일 수정

--- app/src/main/java/com/example/eventtest/Custom.kt
+++ app/src/main/java/com/example/eventtest/Custom.kt
@@ -13,36 +13,13 @@ class Custom : View {
     constructor(_listener: Listener, context: Context?) : super(context) {
         listener = _listener
     }
+
     var str = "yet"
 
-    override fun onAttachedToWindow() {
-        super.onAttachedToWindow()
-        Log.d("heungCustom" , "onAttachedToWindow")
-    }
-
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        Log.d("heungCustom" , "onMeasure")
-    }
-
-    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-        super.onLayout(changed, left, top, right, bottom)
-        Log.d("heungCustom" , "onLayout")
-    }
-
-    override fun layout(l: Int, t: Int, r: Int, b: Int) {
-        super.layout(l, t, r, b)
-        Log.d("heungCustom" , "layout")
-    }
-
-    override fun onDetachedFromWindow() {
-        super.onDetachedFromWindow()
-        Log.d("heungCustom" , "onDetachedFromWindow")
-    }
 
     override fun draw(canvas: Canvas?) {
         super.draw(canvas)
-        Log.d("heungCustom" , "draw")
+        Log.d("myCustom", "draw")
     }
 
     override fun onDraw(canvas: Canvas?) {
@@ -51,29 +28,27 @@ class Custom : View {
         paint.color = Color.BLACK
         paint.textSize = 100F
         canvas!!.drawText(str, 200F, 200F, paint)
-        Log.d("heungCustom" , "onDraw")
+        Log.d("myCustom", "onDraw")
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        when(event!!.action){
+        Log.d("myCustom", "onTouch")
+        when (event!!.action) {
             MotionEvent.ACTION_DOWN -> {
-//                pushMsg()
                 notice("ACTION_DOWN")
-                Log.d("heungCustom", "down")
+                Log.d("myCustom", "down")
             }
             MotionEvent.ACTION_MOVE -> {
-//               moveMsg()
                 notice("ACTION_MOVE")
-                Log.d("heungCustom", "down")
+                Log.d("myCustom", "move")
             }
             else -> {
-//                elseMsg()
                 performClick()
                 notice("ELSE")
-                Log.d("heungCustom", "down")
+                Log.d("myCustom", "else")
             }
         }
-        Log.d("heungCustom", "onTouch")
+        invalidate()
         return true
     }
 
